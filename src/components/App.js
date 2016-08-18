6d7b8f3312df0f82b314fb3d873597acc45387ea
Cade Nichols <c@codinghouse.co>
2016-08-18 10:40:31 -0700

    1.0

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <h1 className="text-center">Welcome to React!</h1>
+    )
+  }
+}
